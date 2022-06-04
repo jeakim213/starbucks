18e0ee01de54779c621e55f10182667e6069ed06
Dan Yi <yidan0908@gmail.com>
2022-06-04 15:08:17 +0900

    dan_0604_15:08_my/index

--- starbucks_clone/src/main/java/com/clone/starbucks/HomeController.java
+++ starbucks_clone/src/main/java/com/clone/starbucks/HomeController.java
@@ -36,47 +36,47 @@ public class HomeController {
 	}
 	
 	//admin
-	@RequestMapping(value="memberListForm")
+	@RequestMapping(value="admin/memberListForm")
 	public String memberListForm() {
 		return "admin/memberListForm";
 	}
 	
-	@RequestMapping(value="userInfoForm")
+	@RequestMapping(value="admin/userInfoForm")
 	public String userInfoForm() {
 		return "admin/userInfoForm";
 	}
 	
-	@RequestMapping(value="modifyCheckForm")
+	@RequestMapping(value="admin/modifyCheckForm")
 	public String modifyCheckForm() {
 		return "admin/modifyCheckForm";
 	}
 	
+	@RequestMapping(value="admin/memberModifyForm")
+	public String memberModifyForm() {
+		return "admin/memberModifyForm";
+	}
+	
+	@RequestMapping(value="admin/deleteCheckForm")
+	public String deleteCheckForm() {
+		return "admin/deleteCheckForm";
+	}
+	
 	//0601 다정 커피
-	@RequestMapping(value="saleChart-1")
+	@RequestMapping(value="admin/saleChart-1")
 	public String saleChart1() {
 		return "admin/saleChart-1";
 	}
 	
 	//0602 다정 푸드
-	@RequestMapping(value="saleChart-2")
+	@RequestMapping(value="admin/saleChart-2")
 	public String saleChart2() {
 		return "admin/saleChart-2";
 	}
 	
 	//프로덕트
-	@RequestMapping(value="saleChart-3")
+	@RequestMapping(value="admin/saleChart-3")
 	public String saleChart3() {
 		return "admin/saleChart-3";
-	}
-	
-	@RequestMapping(value="memberModifyForm")
-	public String memberModifyForm() {
-		return "admin/memberModifyForm";
-	}
-	
-	@RequestMapping(value="deleteCheckForm")
-	public String deleteCheckForm() {
-		return "admin/deleteCheckForm";
 	}
 	
 	// coffee
