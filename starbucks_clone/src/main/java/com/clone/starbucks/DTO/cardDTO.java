bc033113f408d8ff8131d17e79a1d7091743c258
yeeun <inis2510@naver.com>
2022-06-08 14:26:31 +0900

    yeeun_060812_dao정리&dto생성

---
+++ starbucks_clone/src/main/java/com/clone/starbucks/DTO/cardDTO.java
@@ -0,0 +1,77 @@
+package com.clone.starbucks.DTO;
+
+/*
+create table card(
+c_name varchar2(50) not null,
+c_saledate date default sysdate not null,
+id varchar2(30) not null,
+remaincost number,
+c_num varchar2(20) not null unique,
+c_auto number check(c_auto in(0,1)),
+c_master number check(c_master in(0,1))
+);
+
+===== fk =====
+alter table card add foreign key(id) references userinfo(id);
+
+*/
+
+import java.sql.Date;
+
+public class cardDTO {
+	
+	private String c_name;	//카드 이름 [not null]
+	private Date c_saledate;	// 카드 구매일자 [not null, sysdate]
+	private String id;	//아이디 [FK, not null]
+	private int remaincost;	//카드 잔액
+	private String c_num;	// 카드번호[not null, unique]
+	private int c_auto;	// 카드 자동충전 여부 [check]
+	private int c_master;	//주카드 설정 [check]
+	
+	
+	public String getC_name() {
+		return c_name;
+	}
+	public void setC_name(String c_name) {
+		this.c_name = c_name;
+	}
+	public Date getC_saledate() {
+		return c_saledate;
+	}
+	public void setC_saledate(Date c_saledate) {
+		this.c_saledate = c_saledate;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getRemaincost() {
+		return remaincost;
+	}
+	public void setRemaincost(int remaincost) {
+		this.remaincost = remaincost;
+	}
+	public String getC_num() {
+		return c_num;
+	}
+	public void setC_num(String c_num) {
+		this.c_num = c_num;
+	}
+	public int getC_auto() {
+		return c_auto;
+	}
+	public void setC_auto(int c_auto) {
+		this.c_auto = c_auto;
+	}
+	public int getC_master() {
+		return c_master;
+	}
+	public void setC_master(int c_master) {
+		this.c_master = c_master;
+	}
+	
+	
+
+}
---
+++ starbucks_clone/src/main/java/com/clone/starbucks/DTO/registerDTO.java
@@ -0,0 +1,101 @@
+package com.clone.starbucks.DTO;
+
+/*
+create table register(
+id varchar2(30) not null,
+registerdate date default sysdate not null,
+phone varchar2(15) unique not null,
+email varchar2(50) unique not null,
+birthyear number not null,
+birthmonth number not null,
+birthday number not null,
+gender char(1) check(gender in ('M','F')) not null,
+event_e char(1) check(event_e in ('Y','N')) not null,
+event_SMS char(1) check(event_SMS in ('Y','N')) not null
+);
+
+===== fk =====
+alter table register add foreign key(id) references userinfo(id);
+
+
+*/
+
+import java.sql.Date;
+
+public class registerDTO {
+	
+	private String id;	// 아이디 [fK, not null]
+	private Date registerdate; // 가입일자 [not null, sysdate]
+	private String phone;	// 전화번호 [not null, unique]
+	private String email;	// 이메일 [not null, unique]
+	private int birthyear;	// 생년	[not null]
+	private int birthmonth;	// 생월	[not null]
+	private int birthday;	// 생일	[not null]
+	private char gender;	// 성별 [not null, check]
+	private char event_e;	// 이벤트수신허용 - 이메일 [not null, check]
+	private char event_sms;	// 이벤트수신허용 - sms [not null, check]
+
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public Date getRegisterdate() {
+		return registerdate;
+	}
+	public void setRegisterdate(Date registerdate) {
+		this.registerdate = registerdate;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getBirthyear() {
+		return birthyear;
+	}
+	public void setBirthyear(int birthyear) {
+		this.birthyear = birthyear;
+	}
+	public int getBirthmonth() {
+		return birthmonth;
+	}
+	public void setBirthmonth(int birthmonth) {
+		this.birthmonth = birthmonth;
+	}
+	public int getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(int birthday) {
+		this.birthday = birthday;
+	}
+	public char getGender() {
+		return gender;
+	}
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+	public char getEvent_e() {
+		return event_e;
+	}
+	public void setEvent_e(char event_e) {
+		this.event_e = event_e;
+	}
+	public char getEvent_sms() {
+		return event_sms;
+	}
+	public void setEvent_sms(char event_sms) {
+		this.event_sms = event_sms;
+	}
+	
+	
+}
