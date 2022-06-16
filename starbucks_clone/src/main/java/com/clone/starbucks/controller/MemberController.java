96d2530b58455453611e380f8cd3c97add7b2aad
yeeun <inis2510@naver.com>
2022-06-16 14:20:53 +0900

    yeeun_0616_14_my컨트롤러정리(my/index2,3 신경쓰지마세요)

--- starbucks_clone/src/main/java/com/clone/starbucks/controller/MemberController.java
+++ starbucks_clone/src/main/java/com/clone/starbucks/controller/MemberController.java
@@ -24,75 +24,7 @@ import com.google.gson.JsonObject;
 @Controller
 public class MemberController {
 	@Autowired MemberServiceImpl memberService;
-	@Autowired private HttpSession session;
-	
-	// my
-
-		@RequestMapping(value = "my/egiftCard_shopping_bag")
-		public String egiftCard_shopping_bag() {
-			return "my/egiftCard_shopping_bag";
-		}
-
-		@RequestMapping(value = "my/egiftCard")
-		public String egiftCard() {
-			return "my/egiftCard";
-		}
-
-		@RequestMapping(value = "my/eReceiptList")
-		public String eReceiptList() {
-			return "my/eReceiptList";
-		}
-
-		@RequestMapping(value = "my/index")
-		public String my_index() {
-			return "my/index";
-		}
-
-		@RequestMapping(value = "my/mycard_charge")
-		public String mycard_charge() {
-			return "my/mycard_charge";
-		}
-
-		@RequestMapping(value = "my/mycard_charge_1")
-		public String mycard_charge_1() {
-			return "my/mycard_charge-1";
-		}
-
-		@RequestMapping(value = "my/mycard_charge_2")
-		public String mycard_charge_2() {
-			return "my/mycard_charge-2";
-		}
-
-		@RequestMapping(value = "my/mycard_index")
-		public String mycard_index() {
-			return "my/mycard_index";
-		}
-
-		@RequestMapping(value = "my/mycard_info_input")
-		public String mycard_info_input() {
-			return "my/mycard_info_input";
-		}
-
-		@RequestMapping(value = "my/mycard_lost")
-		public String mycard_lost() {
-			return "my/mycard_lost";
-		}
-
-		@RequestMapping(value = "my/mycard")
-		public String mycard() {
-			return "my/mycard";
-		}
-
-		@RequestMapping(value = "my/reward_star_history")
-		public String reward_star_history() {
-			return "my/reward_star_history";
-		}
-
-		@RequestMapping(value = "my/reward")
-		public String reward() {
-			return "my/reward";
-		}
-	
+	@Autowired HttpSession session;
 	
 	
 	//login
